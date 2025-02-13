7af3fb654d87e56602b544d623b14f0e6058d76c
Luigi Piantavinha <szochoralewizc@hotmail.com>
2025-02-13 15:00:58 +0000

    cubd

---
+++ .build/main.d
@@ -0,0 +1,5 @@
+.build/main.o: src/main.c src/../inc/cub3d.h src/../inc/../lib/mlx/mlx.h \
+ src/../inc/../lib/libft/libft_42/libft.h
+src/../inc/cub3d.h:
+src/../inc/../lib/mlx/mlx.h:
+src/../inc/../lib/libft/libft_42/libft.h:
