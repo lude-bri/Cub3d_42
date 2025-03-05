877375b0181c2acfbb871233b2885ec2eaf1ab07
Luigi Piantavinha <szochoralewizc@hotmail.com>
2025-03-05 09:55:09 +0000

    cubd

--- src/210_validate.c
+++ src/210_validate.c
@@ -15,5 +15,9 @@
 int	_validate_data(t_data *data)
 {
 	(void)data;
+	//validar se as texturas existem
+	
+	//validar se os rgb estao corretos
+	//validar se o mapa esta correto
 	return (SUCCESS);
 }
